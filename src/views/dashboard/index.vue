7d0186c1acb92e9141ede09f0e063c67a263bff4
吴怀亮 <9637642+wuhuailiang123@user.noreply.gitee.com>
2025-12-10 22:33:54 +0800

    fix:订单优化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole" />
+    <!-- <component :is="currentRole" /> -->
+    <el-image style="width: 120px; height: 120px;" src="https://file.cardpartner.io/static/images/2025/12/10/b12be47b-ec3d-4652-b12a-fe18b1a1a437.png" alt="logo" />
+    <div class="title">cardpartner 后台管理系统</div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
+// import adminDashboard from './admin'
+// import editorDashboard from './editor'
 
 export default {
   name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
+  // components: { adminDashboard, editorDashboard },
   data() {
     return {
       currentRole: 'adminDashboard'
@@ -29,3 +31,19 @@ export default {
   }
 }
 </script>
+<style scoped>
+.dashboard-container {
+  padding: 32px;
+  background-color: #f3f3f3;
+  min-height: calc(100vh - 84px);
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  font-size: 30px;
+  font-weight: bold;
+}
+.title{
+  margin-top: 20px;
+}
+</style>
